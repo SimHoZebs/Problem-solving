5355b9706a43efc82d1607947e88657033a79d14
SimHoZebs <simhozebs@gmail.com>
2021-12-04 11:39:45 +0900

    move foobar-with-google to its own folder

--- bunny-worker-locations.py
+++
@@ -1,3 +0,0 @@
-def solution(x, y):
-
-    return str(int((y-1)*y/2 + 1 + y*(x-1) + x*(x-1)/2))
--- skipping-work.py
+++
@@ -1,8 +0,0 @@
-def solution(x, y):
-    sum_of_x = sum(x)
-    sum_of_y = sum(y)
-    diff = sum_of_x - sum_of_y
-    if (diff not in x and diff in y) or (diff not in y and diff in x):
-        return diff
-    else:
-        return -diff
