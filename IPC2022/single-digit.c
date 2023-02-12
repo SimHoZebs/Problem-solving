d013770df042ea6da3d46ab68749b22843dbff0e
Sim Ho <simhozebs@gmail.com>
2023-02-11 22:49:16 -0500

    single digit

---
+++ IPC2022/single-digit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define SIZE 999
+
+int re(char num[SIZE], int i)
+{
+
+  if (num[i + 1] <= '9' && num[i + 1] >= '0')
+  {
+    num[i] = '0' + abs(num[i] - num[i + 1]);
+    re(num, i + 1);
+  }
+  else
+  {
+    if (i == 0)
+    {
+      printf("%s", num);
+    }
+    else
+    {
+      num[i] = ' ';
+      re(num, 0);
+    }
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+
+  char num[SIZE];
+  scanf("%s", num);
+  int numLen = strlen(num);
+
+  re(num, 0);
+  return 0;
+}
