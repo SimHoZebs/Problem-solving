6fc6c52fce91d878fc882b930ac7bd3b32337c81
Sim Ho <simhozebs@gmail.com>
2023-02-16 21:38:26 -0500

    reverse array

---
+++ IPC2022/reverse-array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  int arrLen;
+  scanf("%d", &arrLen);
+
+  int *a = malloc(arrLen * sizeof(int));
+
+  for (int i = 0; i < arrLen; i++)
+  {
+    scanf("%d", &a[i]);
+    printf("scanned %d \n", a[i]);
+
+    int *tempArr = malloc(arrLen * sizeof(int));
+    for (int j = i; j >= 0; j--)
+    {
+      printf("Inserting %d to index %d \n", a[j], i - j);
+      tempArr[i - j] = a[j];
+    }
+    free(a);
+    a = tempArr;
+  }
+
+  for (int i = 0; i < arrLen; i++)
+  {
+    printf("%d ", a[i]);
+  }
+
+  return 0;
+}
+
+// Every time an element is added, a new array should be created and become the reverse version of it.
