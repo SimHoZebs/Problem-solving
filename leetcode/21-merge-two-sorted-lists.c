5d2235054334c36085297208695c13c8b09f1856
SimHoZebs <simhozebs@gmail.com>
2023-09-26 10:47:38 -0400

    Leetcode q21

--- leetcode/21-merge-two-sorted-lists.c
+++ leetcode/21-merge-two-sorted-lists.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-// INCOMPLETE SOLUTION
-
 /**
  * Definition for singly-linked list.
  */
@@ -14,30 +12,34 @@ struct ListNode {
 void merge(struct ListNode *node, struct ListNode *l1, struct ListNode *l2) {
   if (!(l1) && !(l2)) return;
 
+  struct ListNode *nextL1 = l1 ? l1->next : NULL;
+  struct ListNode *nextL2 = l2 ? l2->next : NULL;
+
   if (!l1 && l2) {
     node->next = l2;
+    merge(l2, l1, nextL2);
     return;
   } else if (!l2 && l1) {
     node->next = l1;
+    merge(l1, nextL1, l2);
     return;
   }
-
-  struct ListNode *nextL1 = l1->next;
-  struct ListNode *nextL2 = l2->next;
 
   if (l1->val == l2->val) {
     node->next = l1;
     l1->next = l2;
     merge(l2, nextL1, nextL2);
-  }
-  if (l1->val < l2->val) {
+    return;
+  } else if (l1->val < l2->val) {
     node->next = l1;
     l1->next = l2;
     merge(l1, nextL1, l2);
+    return;
   } else {
     node->next = l2;
     l2->next = l1;
     merge(l2, l1, nextL2);
+    return;
   }
 }
 
