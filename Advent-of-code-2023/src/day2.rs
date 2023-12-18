91dab36ef93d8c7a5cbb1626fa190b180b057089
SimHoZebs <simhozebs@gmail.com>
2023-12-18 10:32:04 -0500

    optimzation recommendations by chat gippity

--- Advent-of-code-2023/src/day2.rs
+++ Advent-of-code-2023/src/day2.rs
@@ -1,49 +1,40 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 pub fn part1() {
     println!("part1");
     let input = fs::read_to_string("./src/day2.in").expect("Read day2.in");
 
     //red, green, blue
-    let colors = [12, 13, 14];
+    let mut colors = HashMap::new();
+    colors.insert("red", 12);
+    colors.insert("green", 13);
+    colors.insert("blue", 14);
 
     let mut ans = 0;
 
-    for game in input.lines() {
+    'games: for game in input.lines() {
         let array: Vec<&str> = game.split(':').collect();
 
         //Game<space>
         let game_num = &array[0][5..].parse::<i32>().unwrap();
 
-        let mut is_allowed = true;
-
         for attempt in array[1].split(';') {
             for cube_with_spaces in attempt.split(',') {
                 let num_and_color: Vec<&str> = cube_with_spaces.trim().split(' ').collect();
-                let num = num_and_color[0].parse::<i32>().unwrap();
+                let num = num_and_color[0]
+                    .parse::<i32>()
+                    .expect("Failed to parse number");
                 let color = num_and_color[1];
 
-                let color_index = match color {
-                    "red" => 0,
-                    "green" => 1,
-                    "blue" => 2,
-                    _ => 99,
-                };
-
-                if num > colors[color_index] {
-                    is_allowed = false;
-                    break;
+                if let Some(&color_value) = colors.get(color) {
+                    if num > color_value {
+                        continue 'games;
+                    }
                 }
             }
-
-            if !is_allowed {
-                break;
-            }
         }
 
-        if is_allowed {
-            ans += game_num;
-        }
+        ans += game_num;
     }
 
     println!("{ans}");
