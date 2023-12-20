c251062f53689418eba549313c8a5e812ee985d0
SimHoZebs <simhozebs@gmail.com>
2023-12-20 04:41:33 -0500

    day3 part 1

--- Advent-of-code-2023/src/main.rs
+++ Advent-of-code-2023/src/main.rs
@@ -1,8 +1,9 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 fn main() {
-    let day: u8 = 2;
+    let day: u8 = 3;
 
     match day {
         1 => {
@@ -12,6 +13,9 @@ fn main() {
         2 => {
             day2::part1();
             day2::part2();
+        }
+        3 => {
+            day3::part1();
         }
         _ => println!("Invalid day"),
     }
