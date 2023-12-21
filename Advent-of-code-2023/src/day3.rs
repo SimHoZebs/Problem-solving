44caf2b9e6525f4766c89fb16ed498870acb2e0b
SimHoZebs <simhozebs@gmail.com>
2023-12-20 22:58:31 -0500

    day 3 part 2 solution

--- Advent-of-code-2023/src/day3.rs
+++ Advent-of-code-2023/src/day3.rs
@@ -122,3 +122,146 @@ pub fn part1() {
 
     println!("{answer}");
 }
+
+pub fn part2() {
+    //if pos is "right", then recursively all numbers on the right to the end of array.
+    //if "left", do the same for numbers on the left to the start of array.
+    //otherwise ("mid"), add the number to the array and do both "left" and "right."
+    fn gather_digits(
+        pos: &str,
+        line_iterable: &Vec<char>,
+        line_len: usize,
+        j: usize,
+        digit_array: &mut VecDeque<char>,
+    ) {
+        if line_iterable[j].is_digit(10) {
+            if pos == "left" {
+                digit_array.push_front(line_iterable[j]);
+            } else {
+                digit_array.push_back(line_iterable[j]);
+            }
+        } else {
+            return;
+        }
+
+        if pos != "right" && j > 0 {
+            gather_digits("left", line_iterable, line_len, j - 1, digit_array);
+        }
+
+        if pos != "left" && j < line_len - 1 {
+            gather_digits("right", line_iterable, line_len, j + 1, digit_array);
+        }
+    }
+
+    let input = fs::read_to_string("./src/day3.in").expect("cound't read day3.in");
+
+    let line_array: Vec<(usize, &str)> = input.split("\n").enumerate().collect();
+    let line_array_len = line_array.len();
+
+    let mut answer: u32 = 0;
+
+    for (i, line) in &line_array {
+        for (j, char) in line.char_indices() {
+            //skip everything but gear
+            if char != '*' {
+                continue;
+            }
+
+            //now char is a gear; collect coord of adjcent cells.
+            //The coords will later be used to find numbers.
+            //If non-diagnoal cells have digits, don't care about the diagonal cells on the same line.
+            let mut adj_coords: Vec<[usize; 2]> = vec![];
+
+            //top
+            if *i > 0 {
+                let (adj_i, adj_line) = line_array[*i - 1];
+                let line_iterable: Vec<char> = adj_line.chars().collect();
+
+                //top mid
+                if line_iterable[j].is_digit(10) {
+                    adj_coords.push([adj_i, j]);
+                } else {
+                    //top left
+                    if j > 0 {
+                        adj_coords.push([adj_i, j - 1]);
+                    }
+                    //top right
+                    if j < line_array_len - 1 {
+                        adj_coords.push([adj_i, j + 1]);
+                    }
+                }
+            }
+
+            //left
+            if j > 0 {
+                adj_coords.push([*i, j - 1]);
+            }
+            //right
+            if j < line_array_len {
+                adj_coords.push([*i, j + 1]);
+            }
+
+            //bottom
+            if *i < line_array_len - 1 {
+                let (adj_i, adj_line) = line_array[*i + 1];
+                let line_iterable: Vec<char> = adj_line.chars().collect();
+
+                if line_iterable[j].is_digit(10) {
+                    //bottom mid
+                    adj_coords.push([adj_i, j]);
+                } else {
+                    //bottom left
+                    if j > 0 {
+                        adj_coords.push([adj_i, j - 1]);
+                    }
+                    if j < line_array_len {
+                        adj_coords.push([adj_i, j + 1]);
+                    }
+                }
+            }
+
+            let mut numbers: Vec<u32> = vec![];
+
+            //go through the coords and gather digits in the coords
+            for coord in adj_coords {
+                let mut digit_array: VecDeque<char> = VecDeque::new();
+
+                let (_, adj_line) = line_array[coord[0]];
+                let line_len = adj_line.len();
+                let line_iterable: Vec<char> = adj_line.chars().collect();
+
+                let dir = if coord[1] < j {
+                    "left"
+                } else if coord[1] > j {
+                    "right"
+                } else {
+                    "mid"
+                };
+
+                //gather consecutive digits from the coord
+                gather_digits(dir, &line_iterable, line_len, coord[1], &mut digit_array);
+
+                //the coord never had a digit
+                if digit_array.is_empty() {
+                    continue;
+                }
+
+                let mut number = 0;
+                //starting from oneth number, multiply it by 10^(i)
+                //[1,2,3,4] = 1*10^3 + 2*10^2 + 3*10^1 + 4*10^0 = 1234
+                for (pow, digit) in digit_array.into_iter().rev().enumerate() {
+                    number += (digit.to_digit(10).unwrap()) * (10u32.pow(pow as u32));
+                }
+
+                numbers.push(number);
+            }
+
+            //exactly two part numbers
+            if numbers.len() == 2 {
+                answer += numbers[0] * numbers[1];
+            }
+        }
+    }
+
+    println!("{answer}");
+}
